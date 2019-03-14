fbf8922b407f7b4194a342329c0bf8cef423f85a
Abhish Abraham <abhish002@gmail.com>
2019-03-14 00:01:22 -0500

    added styles for login page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import { startLogout } from '../actions/auth';
 import { connect } from 'react-redux';
 
 export const Header = ({ startLogout }) => (
-    <header>
-        <h1>Expensify</h1>        
-        <NavLink to="/dashboard" activeClassName="is-active" >Home</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
+    <header className="header">
+    <div className="content-container">
+        <Link className="header__title" to="/dashboard">
+            <h1>Expensify</h1>
+        </Link>
+        {/*<NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>*/}
         {/* <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink> */}
-        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        {/*<NavLink to="/help" activeClassName="is-active">Help</NavLink>*/}
         <button onClick = { startLogout }>Logout</button>
+    </div>        
     </header>
 );
 
